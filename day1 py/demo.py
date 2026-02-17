8ae45064c742cb177db0895fdf41d07c04b1577b
arathi5673 <arathisanthosh133@gmail.com>
2026-02-17 10:13:19 +0530

    Add files via upload

---
+++ day1 py/demo.py
@@ -0,0 +1,100 @@
+_aPple=10
+# print(_aPple)
+
+# Datatype 4 type
+# int,float,bollean,string
+# a+++....>a=a+1  other language
+#a= a+1  > a+=1
+
+# #task
+# birth_year=1995
+# current_year=2026
+# print(birth_year-current_year)
+
+# task2
+# current_age =20
+# maximun_age =55
+# estm_amount =500
+# print(estm_amount*current_age)
+
+#task3
+# current_age =20
+# maximun_age =40
+# water_drink =2
+# x=maximun_age-current_age
+# y=x*365
+# z=y*2
+# print(z)
+
+# task 4
+
+# distance =1000
+# mileasge = 40
+# petrol = 100
+# x= distance/ mileasge
+# print(x)
+# y= x*petrol
+# print(y)
+
+#task 5
+# data_usage =2
+# days_month =31
+# x=data_usage*days_month
+# print(x)
+
+#task 6
+# current_age=20
+# year=365
+# x=(year*current_age)
+# print(x)
+
+# task 7
+# original_price=5000
+# discount =5
+# x=discount/100*original_price
+# print(x)
+# y=original_price-x
+# print(y)
+
+# #task 7
+# x=6
+# y=8
+# if x<y:
+#     print("x is less then y")
+#  else:
+#     print("y is gerater then x")
+
+# #task 8
+# striing=input("enter the striing")
+# if x="a"or "e"or "i"or "o"or "u":
+#     print("vowel")
+# else:
+#     print("consonant")
+
+# task9
+
+# marks=int(input("enter the value "))
+# if marks>=100:
+#     print("invaida grade")
+# else:
+#     print("a grade")
+
+#task 10
+# y=60
+# s=4
+# if s<y:
+#     print("profit")
+# else:
+#     print("loss")
+
+
+
+
+
+
+
+  
+      
+
+    
+
